702e251612790b2218db152d808349dc0cff4554
ashley mark fletcher <ashleymarkfletcher@gmail.com>
2019-07-03 17:54:41 +0100

    Comment and use the npm package

--- src/index.js
+++ src/index.js
@@ -10,18 +10,16 @@ const fd = Scene.root
 const planeTracker = Scene.root.child('planeTracker0')
 const TouchGestures = require('TouchGestures')
 
+// cannon is still needed as a direct import because I need at add CANNON as a static property on the module
 import CANNON from 'cannon'
-import CannonHelper from './cannonHelper'
-
-const rangeMap = (input, inLow, inHigh, outLow, outHigh) => {
-  return Math.round(((input - inLow) / (inHigh - inLow)) * (outHigh - outLow) + outLow)
-}
+import CannonHelper from 'spark-ar-physics'
 
 // show switch camera instructions on front camera
 Instruction.bind(CameraInfo.captureDevicePosition.eq(CameraInfo.CameraPosition.FRONT), 'flip_camera')
 
 var floorPlane = planeTracker.child('plane0')
-// get all the pins
+
+// get all the pins, why did I name them like that...
 var ball = planeTracker.child('BowlingBall')
 var pin = planeTracker.child('Bowling_Pin')
 var pin2 = planeTracker.child('Bowling_Pin0')
@@ -36,9 +34,6 @@ var pin10 = planeTracker.child('Bowling_Pin8')
 
 var pins = [pin, pin2, pin3, pin4, pin5, pin6, pin7, pin8, pin9, pin10]
 
-// var world = new CANNON.World()
-// world.gravity.set(0, -19.82, 0) // m/s²
-
 // Create a sphere
 var radius = 6 // m
 var sphereBody = new CANNON.Body({
@@ -47,25 +42,23 @@ var sphereBody = new CANNON.Body({
   shape: new CANNON.Sphere(radius)
 })
 
+// helper for making my pins
 function initPin(pos) {
   var pinBody = new CANNON.Body({
     mass: 0.2,
     position: pos,
     shape: new CANNON.Box(new CANNON.Vec3(2, 10, 2))
-    // DYNAMIC: 1
-    // fixedRotation: true
-    // linearDamping: 0.1,
-    // angularDamping: 0.5
   })
-  //   world.addBody(pinBody)
+
   return pinBody
 }
 
+// create the cannon floor
 var floor = CannonHelper.makeFloor()
 
+// get the initial world objects needed
 var worldObjects = [{ sceneObject: floorPlane, physicsObject: floor }, { sceneObject: ball, physicsObject: sphereBody }]
 
-// ----------------------------- create the pins -------------------------------
 // set the base position for the pins
 var initialPinX = 0
 var initialPinY = 10
@@ -91,25 +84,36 @@ function initPinPos() {
   ]
 }
 
+// init the positions for the pins
 var pinPos = initPinPos()
+
+// create a new worldObject for each pin
 pins.forEach((pin, i) => {
   worldObjects.push({ sceneObject: pin, physicsObject: initPin(pinPos[i]) })
 })
 
+// init the cannon world
 var cannonHelper = new CannonHelper(worldObjects)
 
+// create a game loop
 var loopTimeMs = 30
 var lastTime
+
+// couldn't really think of a better way so an interval it is...
 Time.ms.interval(loopTimeMs).subscribe(function(elapsedTime) {
   if (lastTime !== undefined) {
     var deltaTime = (elapsedTime - lastTime) / 1000
-    // world.step(fixedTimeStep, deltaTime, maxSubSteps)
 
+    // update the cannon world each loop
     cannonHelper.update(deltaTime)
   }
 
   lastTime = elapsedTime
 })
+
+const rangeMap = (input, inLow, inHigh, outLow, outHigh) => {
+  return Math.round(((input - inLow) / (inHigh - inLow)) * (outHigh - outLow) + outLow)
+}
 
 TouchGestures.onTap().subscribe(function(e) {
   // convert the x of the tap to an x for force later
@@ -119,6 +123,8 @@ TouchGestures.onTap().subscribe(function(e) {
 })
 
 function resetGame() {
+  // zero everything out to make them static again
+  // you can't just set the postiion and rotation because they will still have forces applied to them
   sphereBody.position = new CANNON.Vec3(0, 5, 0)
   sphereBody.angularVelocity = new CANNON.Vec3(0, 0, 0)
   sphereBody.velocity = new CANNON.Vec3(0, 0, 0)
@@ -139,7 +145,6 @@ function resetGame() {
 
 var resetTimer
 var thrown = false
-
 function throwBall(xDirection) {
   if (thrown) return
 
